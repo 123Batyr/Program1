99b2cebd504ab90cdb3448de09437c7166ac446d
BatyrM <meredovbatyr2001@gmail.com>
2023-05-12 17:49:46 +0500

    PostgreSQL-connect

---
+++ src/main/java/ru/Meredov/Simple/JDBCExample.java
@@ -0,0 +1,25 @@
+package ru.Meredov.Simple;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCExample {
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://127.0.0.1:5432/test", "postgres", "9920007784")) {
+
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
