89f6883b4c08e00f08e3b66eae7e54410f2e3e52
BatyrM <meredovbatyr2001@gmail.com>
2023-05-13 14:15:20 +0500

    PostgreSQL-connect2

--- src/main/java/ru/Meredov/Simple/Main.java
+++ src/main/java/ru/Meredov/Simple/Main.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class JDBCExample {
+public class Main {
     public static void main(String[] args) {
         String dbUrl = "jdbc:postgresql://" + args[0] + ":" + args[1] + "/" + args[2];
         String username = args[3];
